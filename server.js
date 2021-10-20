5b911d589517bf5e3f3863f00a0ed9530b9d5b87
colton bloomquist <coltonbloomquist@yahoo.com>
2021-10-20 16:03:13 -0600

    added vote and comment functionality. Refactored using partials

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+const helpers = require("./utils/helpers");
 
 const session = require("express-session");
 
